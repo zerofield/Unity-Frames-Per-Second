78f901cf35f56a30fb853bab42a200036f5698b1
Jin Xu <xujin@i-vsun.com>
2016-07-11 10:29:56 +0800

    added plane model

---
+++ Assets/Scripts/Nucleon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour
+{
+
+    public float attractionForce;
+
+    Rigidbody body;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        //pulls the nucleon towards the origin
+        body.AddForce(transform.localPosition * -attractionForce);
+    }
+
+
+}
---
+++ Assets/Scripts/NucleonSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NucleonSpawner : MonoBehaviour
+{
+
+    public float timeBetweenSpawns;
+
+    public float spawnDistance;
+
+    public Nucleon[] nucleonPrefabs;
+
+    float timeSinceLastSpawn;
+
+    void FixedUpdate()
+    {
+
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (timeSinceLastSpawn > timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeSinceLastSpawn;
+            SpawnNucleon();
+        }
+    }
+
+
+    void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+
+    }
+
+
+}
